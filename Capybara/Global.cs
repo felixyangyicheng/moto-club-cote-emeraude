9aec027574eb8ec551a8b5773ae19284c499111b
felixyangyicheng <felix.yang.yicheng@gmail.com>
2025-02-14 10:54:03 +0100

    clear

--- Capybara/Global.cs
+++ Capybara/Global.cs
@@ -4,7 +4,7 @@ global using System.Diagnostics.CodeAnalysis;
 global using System.Linq;
 global using System.Threading.Tasks;
 global using Microsoft.AspNetCore.Components;
-global using Toolbelt.Blazor.SpeechSynthesis;
+
 global using System.Runtime.InteropServices;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -13,13 +13,11 @@ global using System.Text;
 
 global using MudBlazor.Services;
 global using MudBlazor;
-global using Toolbelt.Blazor.Extensions.DependencyInjection;
-global using Tewr.Blazor.FileReader;
+
 global using Capybara.Models;
 global using System.Net.Http.Json;
-global using ApexCharts;
 
-global using JiuLing.CommonLibs.ExtensionMethods;
+
 global using Microsoft.AspNetCore.SignalR.Client;
 global using Microsoft.JSInterop;
 global using Capybara.Models.Enums;
@@ -30,8 +28,6 @@ global using System.Text.Json;
 global using Capybara.Shared;
 global using Capybara.Services;
 
-global using Heron.MudCalendar;
-global using ZXingBlazor.Components;
 
 global using Color = MudBlazor.Color;
 global using Console = System.Console;
