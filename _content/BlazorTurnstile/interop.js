5f69872040c1e5d888cdb61c6353f1fd34da9a1d
unknown <felixyangyicheng@gmail.com>
2025-06-23 10:41:55 +0200

    purge gh-pages

--- _content/BlazorTurnstile/interop.js
+++
@@ -1,6 +0,0 @@
-export function render(obj, element, parameters) {
-    parameters['callback'] = (token) => obj.invokeMethodAsync('Callback', token);
-    parameters['error-callback'] = (message) => obj.invokeMethodAsync('ErrorCallback', message);
-
-    return turnstile.render(element, parameters);
-}
